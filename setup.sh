145554d4dd48d1d1b96aa1e0a62bf8f748f40ad0
Anik De <anekde@gmail.com>
2025-02-12 18:00:10 +0530

    name change

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ conda create -n "$ENV_NAME" python=3.9 -y
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "$ENV_NAME"
 
-# Clone the repository and navigate into it
-echo "Cloning the IndicPhotoOCR repository..."
-git clone https://github.com/Bhashini-IITJ/IndicPhotoOCR.git
-cd IndicPhotoOCR || exit
+# # Clone the repository and navigate into it
+# echo "Cloning the IndicPhotoOCR repository..."
+# git clone https://github.com/Bhashini-IITJ/IndicPhotoOCR.git
+# cd IndicPhotoOCR || exit
 
 # Install pip-tools and generate requirements files
 #echo "Installing pip-tools and generating requirements files..."
@@ -33,8 +33,8 @@ cd IndicPhotoOCR || exit
 #pip install lmdb==1.5.1
 
 # Build and install the project
-echo "Building and installing BharatOCR..."
+echo "Building and installing IndicPhotoOCR..."
 python setup.py sdist bdist_wheel
-pip install ./dist/IndicPhotoOCR-1.2.0-py3-none-any.whl[cu118] --extra-index-url https://download.pytorch.org/whl/cu118
+pip install ./dist/IndicPhotoOCR-1.3.0-py3-none-any.whl[cu118] --extra-index-url https://download.pytorch.org/whl/cu118
 
 echo "Installation complete!"
